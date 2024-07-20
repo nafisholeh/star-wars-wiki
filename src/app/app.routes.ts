0224bd75973d75f67a0610575a243d00312c2cf6
nafisholeh <rachmadnafisholeh@gmail.com>
2024-07-20 15:14:07 +0700

    feat: add star war character lists page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { PeopleListComponent } from './components/people-list/people-list.component';
+import { PeopleDetailComponent } from './components/people-detail/people-detail.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: PeopleListComponent },
+  { path: 'people/:id', component: PeopleDetailComponent },
+  { path: '**', redirectTo: '', pathMatch: 'full' }  // Redirect any unknown paths to the main page
+];
--- src/app/components/people-list/people-list.component.ts
+++ src/app/components/people-list/people-list.component.ts
@@ -1,12 +1,48 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { SwapiService } from '@app/services/swapi.service';
+import { PeopleResponse, Person } from '@models/swapi.types';
 
 @Component({
   selector: 'app-people-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './people-list.component.html',
   styleUrl: './people-list.component.scss'
 })
-export class PeopleListComponent {
+export class PeopleListComponent implements OnInit {
 
+  people: Person[] = [];
+  next: string | null = null;
+  previous: string | null = null;
+  page: number = 1;
+
+  constructor(private swapiService: SwapiService) { }
+
+  ngOnInit(): void {
+    this.fetchPeople();
+  }
+
+  fetchPeople(): void {
+    this.swapiService.getPeople(this.page).subscribe((data: PeopleResponse) => {
+      this.people = data.results;
+      this.next = data.next;
+      this.previous = data.previous;
+    })
+  }
+
+  nextPage(): void {
+    if (this.next) {
+      this.page++;
+      this.fetchPeople();
+    }
+  }
+
+  previousPage(): void {
+    if (this.previous) {
+      this.page--;
+      this.fetchPeople();
+    }
+  }
 }
