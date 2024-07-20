87545280161c226eaec788be78ee9610d0e39df0
nafisholeh <rachmadnafisholeh@gmail.com>
2024-07-20 12:01:05 +0700

    feat: generate SwapiService, PeopleListComponent, PeopleDetailComponent

---
+++ src/app/components/people-detail/people-detail.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-people-detail',
+  standalone: true,
+  imports: [],
+  templateUrl: './people-detail.component.html',
+  styleUrl: './people-detail.component.scss'
+})
+export class PeopleDetailComponent {
+
+}
